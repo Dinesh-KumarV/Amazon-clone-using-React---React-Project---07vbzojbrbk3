2d226e4deffa40d9704a5699ba97b1c9674a24f9
User <dineshark33@gmail.com>
2022-12-12 21:39:17 +0530

    commited

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+
+import Orders from "./Orders";
+
+import { useStateValue } from "./StateProvider";
+
+function App() {
+  const [{}, dispatch] = useStateValue();
+
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
